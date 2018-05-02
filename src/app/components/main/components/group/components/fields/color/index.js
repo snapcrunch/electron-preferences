bb587792a564c3e561454f07521d0b2261089abb
inventivetalent <mail@inventivetalent.org>
2018-05-02 20:16:04 +0200

    change color picker to pop up when clicking on the selected color

--- src/app/components/main/components/group/components/fields/color/index.js
+++ src/app/components/main/components/group/components/fields/color/index.js
@@ -3,21 +3,53 @@
 import React from 'react';
 import './style.scss';
 import { ChromePicker } from "react-color";
+import * as ReactDOM from "react-dom";
 
 class ColorField extends React.Component {
 
-    state = {};
+    state = {
+        displayColorPicker: false
+    };
 
     render() {
 
         return (
             <div className="field field-color">
                 <div className="field-label">{ this.label }</div>
-                <ChromePicker color={ this.value } onChange={ this.onChange.bind(this) } disableAlpha={ this.format === "hex" }/>
-                { this.help && <span className="help">{ this.help }</span> }
+                <div className="color-swatch" onClick={ this.handleClick }>
+                    <div className="color" style={ this.style }/>
+                </div>
+                { this.state.displayColorPicker ? <div className="color-popover">
+                    <ChromePicker color={ this.value } onChange={ this.onChange.bind(this) } disableAlpha={ this.format === "hex" }/>
+                </div> : null }
             </div>
         );
 
+    }
+
+
+    handleClick = () => {
+        this.setState({displayColorPicker: !this.state.displayColorPicker})
+    };
+
+    handleClose = () => {
+        this.setState({displayColorPicker: false})
+    };
+
+    componentDidMount() {
+        document.addEventListener('click', this.handleClickOutside, true);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClickOutside, true);
+    }
+
+    handleClickOutside = (event) => {
+        const domNode = ReactDOM.findDOMNode(this);
+
+        if (!domNode || !domNode.contains(event.target)) {
+            this.handleClose();
+        }
     }
 
     get field() {
@@ -47,6 +79,22 @@ class ColorField extends React.Component {
     get format() {
 
         return this.field.format;
+
+    }
+
+    get style() {
+
+        let style = "";
+        if (this.format === "rgb") {
+            style = `rgba(${ this.value.r }, ${ this.value.g }, ${ this.value.b }, ${ this.value.a })`
+        } else if (this.format === "hex") {
+            style = this.value;
+        } else if (this.format === "hsl") {
+            style = `hsla(${ this.value.h }, ${ this.value.s * 100 }%, ${ this.value.l * 100 }%, ${ this.value.a })`
+        } else if (this.value.hex) {
+            style = this.value.hex;
+        }
+        return {background: style};
 
     }
 
