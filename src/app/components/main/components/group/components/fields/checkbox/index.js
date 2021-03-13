43f91c59e7d0e091bb98320f6db2642e170b64db
Pieter-Jan Van Robays <pieterjan.vanrobays@gmail.com>
2021-03-13 18:55:57 +0100

    update UI of the checkbox component add animation to the checkbox component

--- src/app/components/main/components/group/components/fields/checkbox/index.js
+++ src/app/components/main/components/group/components/fields/checkbox/index.js
@@ -15,9 +15,11 @@ class CheckboxField extends React.Component {
         const options = this.options.map((option, idx) => {
             const id = `${fieldID}_${idx}`;
             return (
-                <div className="checkbox-option" key={ idx }>
-                <input type="checkbox" id={ id } onChange={ this.onChange.bind(this) } checked={ this.value.indexOf(option.value) >= 0 } /> <label htmlFor={ id }>{ option.label }</label>
-                </div>
+                <label htmlFor={ id } className="checkbox-option">
+                    { option.label }
+                    <input type="checkbox" id={ id } onChange={ this.onChange.bind(this) } checked={ this.value.indexOf(option.value) >= 0 } />
+                    <span className="check-square" />
+                </label>
             );
         });
 
