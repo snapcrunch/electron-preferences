c493d1385677b6e01f36e929bd6e0270d9d2f209
Lacy Morrow <me@lacymorrow.com>
2021-07-07 14:24:56 -0700

    rewrite as a functional component

--- src/app/components/main/components/group/components/fields/accelerator/index.js
+++ src/app/components/main/components/group/components/fields/accelerator/index.js
@@ -2,103 +2,55 @@
 
 import React from 'react';
 
-class AcceleratorField extends React.Component {
+const AcceleratorField = ({field, value, onChange, ...props}) => {
+  	const [keys, setKeys] = React.useState([]);
+  	const [keyCount, setKeyCount] = React.useState(0);
 
-    state = {};
+	const handleKeyDown = (event) => {
 
-    constructor(props) {
-        
-        super(props);
-        
-        this.pressedKeys = [];
-        this.keyCount = 0;
-        this.handleKeyDown = this.handleKeyDown.bind(this);
-        this.handleKeyUp = this.handleKeyUp.bind(this);
-        this.mapAccelerator = this.mapAccelerator.bind(this);
-        this.onChange = this.onChange.bind(this);
-        
-    }
-
-    render() {
-
-        return (
-            <div className="field field-accelerator">
-                <div className="field-label">{ this.label }</div>
-                <input type="text" value={ this.value } onKeyDown={ this.handleKeyDown } onKeyUp={ this.handleKeyUp } readOnly />
-                { this.help && <span className="help">{ this.help }</span> }
-            </div>
-        );
-
-    }
-
-    handleKeyDown(event) {
-        
         event.preventDefault();
 
-        if (this.keyCount <= 0) {
-            this.pressedKeys = [];
+        if (keyCount <= 0) {
+            setKeys([]);
         }
 
-        if (this.pressedKeys.indexOf(event.key) === -1) {
-            this.pressedKeys.push(event.key);
-            this.keyCount++;
+        if (keys.indexOf(event.key) === -1) {
+            keys.push(event.key);
+            setKeyCount(keyCount + 1);
         }
-        
-        this.mapAccelerator(this.pressedKeys);
-        
+
+        mapAccelerator(keys);
+
     }
 
-    handleKeyUp(event) {
-        
+    const handleKeyUp = (event) => {
+
         event.preventDefault();
-        this.keyCount--;
-        this.mapAccelerator(this.pressedKeys);
-        
+        setKeyCount(keyCount - 1);
+        mapAccelerator(keys);
+
     }
 
-    mapAccelerator(keys) {
-        
+    const mapAccelerator = (keys) => {
+
         const arr = keys.map((item) => {
             if (item === ' ') return "Space";
             if (item === '+') return "Plus";
             if (/^[a-z]$/.test(item)) return item.toUpperCase();
             return item;
         });
-        
-        this.onChange(arr.join("+"));
-        
-    }
 
-    get field() {
-
-        return this.props.field;
+        onChange(arr.join("+"));
 
     }
 
-    get value() {
-
-        return this.props.value || '';
-
-    }
-
-    get label() {
-
-        return this.field.label;
-
-    }
-
-    get help() {
-
-        return this.field.help;
-
-    }
-
-    onChange(value) {
-
-        return this.props.onChange(value);
-
-    }
-
-}
+  return (
+        <div className="field field-accelerator">
+            <div className="field-label">{ field.label }</div>
+            <input type="text" value={ value || '' } onKeyDown={ handleKeyDown } onKeyUp={ handleKeyUp } readOnly />
+            { field.help && <span className="help">{ field.help }</span> }
+        </div>
+  );
+};
 
 export default AcceleratorField;
