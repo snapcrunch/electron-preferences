571f16ef05de80a7186d67184b1843845c8b2d06
Lacy Morrow <me@lacymorrow.com>
2021-07-07 19:46:05 -0700

    adding eslint and my personal rules, base on XO

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,63 @@
-'use strict';
+'use strict'
 
-const webpack = require('webpack');
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require( 'path' )
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
 
 module.exports = {
-    'mode': 'production',
-    'entry': './src/app/index.js',
-    'watchOptions': {
-        'ignored': /node_modules/
-    },
-    'output': {
-        'path': path.resolve(__dirname, 'build'),
-        'filename': 'app.js'
-    },
-    'resolve': {
-        'extensions': [
-            '.js', '.scss'
-        ],
-        'modules': [
-            path.resolve(__dirname, 'src'),
-            path.resolve(__dirname, 'scss'),
-            path.resolve(__dirname, 'node_modules')
-        ],
-        'alias': {
-        }
-    },
-    'plugins': [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'assets' }
-            ]
-        })
-    ],
-    'module': {
-        'rules': [
-            {
-                'test': /\.js$/,
-                'exclude': /node_modules/,
-                'use': [
-                    {
-                        'loader': 'cache-loader'
-                    },
-                    {
-                        'loader': 'babel-loader',
-                        'options': {
-                            'presets': ['env', 'react'],
-                            'plugins': ['transform-object-rest-spread', 'transform-class-properties']
-                        }
-                    }
-                ]
-            },
-            {
-                'test': /\.scss$/,
-                'use': [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            }
-        ]
-    }
-};
+	mode: 'production',
+	entry: './src/app/index.js',
+	watchOptions: {
+		ignored: /node_modules/,
+	},
+	output: {
+		path: path.resolve( __dirname, 'build' ),
+		filename: 'app.js',
+	},
+	resolve: {
+		extensions: [
+			'.js', '.scss',
+		],
+		modules: [
+			path.resolve( __dirname, 'src' ),
+			path.resolve( __dirname, 'scss' ),
+			path.resolve( __dirname, 'node_modules' ),
+		],
+		alias: {
+		},
+	},
+	plugins: [
+		new CopyWebpackPlugin( {
+			patterns: [
+				{ from: 'assets' },
+			],
+		} ),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'cache-loader',
+					},
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ 'env', 'react' ],
+							plugins: [ 'transform-object-rest-spread', 'transform-class-properties' ],
+						},
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+}
