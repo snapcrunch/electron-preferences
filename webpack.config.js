2e21171d9de5a00fedb3dcc0154683bbe9b46bf3
Lacy Morrow <me@lacymorrow.com>
2021-07-11 20:13:32 -0700

    adding dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,60 +3,62 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-	mode: 'development',
-	entry: './src/app/index.jsx',
-	watchOptions: {
-		ignored: /node_modules/,
-	},
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'app.js',
-	},
-	resolve: {
-		extensions: [
-			'.js', '.jsx', '.scss',
-		],
-		modules: [
-			path.resolve(__dirname, 'src'),
-			path.resolve(__dirname, 'scss'),
-			path.resolve(__dirname, 'node_modules'),
-		],
-		alias: {
+module.exports = (env, argv) => {
+	return {
+		mode: 'production',
+		entry: './src/app/index.jsx',
+		watchOptions: {
+			ignored: /node_modules/,
 		},
-	},
-	plugins: [
-		new CopyWebpackPlugin({
-			patterns: [
-				{ from: 'assets' },
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'app.js',
+		},
+		resolve: {
+			extensions: [
+				'.js', '.jsx', '.scss',
 			],
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'cache-loader',
-					},
-					{
-						loader: 'babel-loader',
-						options: {
-							presets: [ '@babel/preset-env', '@babel/preset-react' ],
-						},
-					},
-				],
+			modules: [
+				path.resolve(__dirname, 'src'),
+				path.resolve(__dirname, 'scss'),
+				path.resolve(__dirname, 'node_modules'),
+			],
+			alias: {
 			},
-			{
-				test: /\.scss$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader',
+		},
+		plugins: [
+			new CopyWebpackPlugin({
+				patterns: [
+					{ from: 'assets' },
 				],
-			},
+			}),
 		],
-	},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'cache-loader',
+						},
+						{
+							loader: 'babel-loader',
+							options: {
+								presets: [ '@babel/preset-env', '@babel/preset-react' ],
+							},
+						},
+					],
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						'style-loader',
+						'css-loader',
+						'sass-loader',
+					],
+				},
+			],
+		}
+	}
 };
