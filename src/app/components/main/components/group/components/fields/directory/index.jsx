e72372d5840813cb4ce9ea07f2c2c1cf7f418afc
Lacy Morrow <me@lacymorrow.com>
2021-07-11 19:54:00 -0700

    lint

--- src/app/components/main/components/group/components/fields/directory/index.jsx
+++ src/app/components/main/components/group/components/fields/directory/index.jsx
@@ -1,108 +1,163 @@
-'use strict'
+/* global api */
+'use strict';
 
 import React from 'react';
-import {isArray} from "../../../../../../../utils/isArray";
+import PropTypes from 'prop-types';
+import { isArray } from '../../../../../../../utils/isArray';
 
 class DirectoryField extends React.Component {
 
-    render() {
-        const { multiSelections, value, help, label } = this;
+	render() {
 
-        const btLabel = value && value.length > 0
-            ? (multiSelections ? 'Choose other Folders' : 'Choose Another Folder')
-            : (multiSelections ? 'Choose Folders' : 'Choose a Folder');
+		const { multiSelections, value, help, label } = this;
 
-        return (
-            <div className="field field-directory">
-                <div className="field-label">{ label }</div>
-                <div className="value" onClick={ this.choose }>
-                    {multiSelections ? "Folders" : "Folder"}:&nbsp;
-                    {
-                        value
-                            ? (
-                                multiSelections || value.length > 1
-                                    ? <ul>{value.map(v => <li>{v}</li>)}</ul>
-                                    : value[0]
-                            )
-                            : 'None'
-                    }
-                </div>
-                <div className="bt" onClick={ this.choose }>
-                    { btLabel }
-                </div>
-                { help && <span className="help">{ help }</span> }
-            </div>
-        );
+		const btLabel = value && value.length > 0
+			? (multiSelections ? 'Choose other Folders' : 'Choose Another Folder')
+			: (multiSelections ? 'Choose Folders' : 'Choose a Folder');
+
+		return (
+			<div className="field field-directory">
+				<div className="field-label">{ label }</div>
+				<div className="value" onClick={ this.choose }>
+					{multiSelections ? 'Folders' : 'Folder'}:&nbsp;
+					{
+						value
+							? (
+								multiSelections || value.length > 1
+									? <ul>{value.map((v, i) => <li key={i}>{v}</li>)}</ul>
+									: value[0]
+							)
+							: 'None'
+					}
+				</div>
+				<div className="bt" onClick={ this.choose }>
+					{ btLabel }
+				</div>
+				{ help && <span className="help">{ help }</span> }
+			</div>
+		);
 
 	}
 
-    get field() {
-        return this.props.field;
-    }
+	get field() {
 
-    get value() { //Always return an array
-        const { value } = this.props;
-        if (typeof(value) === "undefined")
-            return undefined;
-        return isArray(value) ? value : [value];
-    }
+		return this.props.field;
 
-    get label() {
-        return this.field.label;
-    }
+	}
 
-    get type() {
-        return this.field.type;
-    }
+	get value() { // Always return an array
 
-    get help() {
-        return this.field.help;
-    }
+		const { value } = this.props;
+		if (typeof (value) === 'undefined') {
 
-    get multiSelections() {
-        return this.field.multiSelections || false;
-    }
+			return undefined;
 
-    get noResolveAliases() {
-        return this.field.noResolveAliases || false;
-    }
+		}
 
-    get treatPackageAsDirectory() {
-        return this.field.treatPackageAsDirectory || false;
-    }
+		return isArray(value) ? value : [ value ];
 
-    get dontAddToRecent() {
-        return this.field.dontAddToRecent || false;
-    }
+	}
 
-    get onChange() {
-        return this.props.onChange;
-    }
+	get label() {
 
-    choose = () => {
-        const { multiSelections, noResolveAliases, treatPackageAsDirectory, dontAddToRecent } = this;
-        const properties = ['openDirectory', 'createDirectory'];
-        if (multiSelections)
-            properties.push("multiSelections");
-        if (noResolveAliases)
-            properties.push("noResolveAliases");
-        if (treatPackageAsDirectory)
-            properties.push("treatPackageAsDirectory");
-        if (dontAddToRecent)
-            properties.push("dontAddToRecent");
+		return this.field.label;
 
-        const result = api.showOpenDialog({
-            properties: properties
-        });
+	}
 
-        if (!result)
-            return;
+	get type() {
 
-        if (result.length) {
-            this.onChange(multiSelections ? result : result[0]);
-        }
-    }
+		return this.field.type;
+
+	}
+
+	get help() {
+
+		return this.field.help;
+
+	}
+
+	get multiSelections() {
+
+		return this.field.multiSelections || false;
+
+	}
+
+	get noResolveAliases() {
+
+		return this.field.noResolveAliases || false;
+
+	}
+
+	get treatPackageAsDirectory() {
+
+		return this.field.treatPackageAsDirectory || false;
+
+	}
+
+	get dontAddToRecent() {
+
+		return this.field.dontAddToRecent || false;
+
+	}
+
+	get onChange() {
+
+		return this.props.onChange;
+
+	}
+
+	choose() {
+
+		const { multiSelections, noResolveAliases, treatPackageAsDirectory, dontAddToRecent } = this;
+		const properties = [ 'openDirectory', 'createDirectory' ];
+		if (multiSelections) {
+
+			properties.push('multiSelections');
+
+		}
+
+		if (noResolveAliases) {
+
+			properties.push('noResolveAliases');
+
+		}
+
+		if (treatPackageAsDirectory) {
+
+			properties.push('treatPackageAsDirectory');
+
+		}
+
+		if (dontAddToRecent) {
+
+			properties.push('dontAddToRecent');
+
+		}
+
+		const result = api.showOpenDialog({
+			properties,
+		});
+
+		if (!result) {
+
+			return;
+
+		}
+
+		if (result.length) {
+
+			this.onChange(multiSelections ? result : result[0]);
+
+		}
+
+	}
 
 }
 
-export default DirectoryField
+DirectoryField.propTypes = {
+	field: PropTypes.object,
+	value: PropTypes.string,
+	onChange: PropTypes.func,
+};
+
+export default DirectoryField;
--- src/app/components/main/components/group/components/fields/file/index.jsx
+++ src/app/components/main/components/group/components/fields/file/index.jsx
@@ -1,118 +1,182 @@
+/* global api */
 'use strict';
 
 import React from 'react';
-import {isArray} from "../../../../../../../utils/isArray";
+import PropTypes from 'prop-types';
+import { isArray } from '../../../../../../../utils/isArray';
 
 class FileField extends React.Component {
-    render() {
-        const { multiSelections, value, help, label } = this;
 
-        const btLabel = value && value.length > 0
-            ? (multiSelections ? 'Choose other Files' : 'Choose another File')
-            : (multiSelections ? 'Choose Files' : 'Choose a File');
+	render() {
 
-        return (
-            <div className="field field-file">
-                <div className="field-label">{label}</div>
-                <div className="value" onClick={this.choose}>
-                    {multiSelections ? "Files" : "File"}:&nbsp;
-                    {
-                        value
-                            ? (
-                                multiSelections || value.length > 1
-                                    ? <ul>{value.map(v => <li>{v}</li>)}</ul>
-                                    : value[0]
-                            )
-                            : 'None'
-                    }
-                </div>
-                <div className="bt" onClick={this.choose}>
-                    {btLabel}
-                </div>
-                {help && <span className="help">{help}</span>}
-            </div>
-        );
+		const { multiSelections, value, help, label } = this;
 
-    }
+		const btLabel = value && value.length > 0
+			? (multiSelections ? 'Choose other Files' : 'Choose another File')
+			: (multiSelections ? 'Choose Files' : 'Choose a File');
 
-    get field() {
-        return this.props.field;
-    }
+		return (
+			<div className="field field-file">
+				<div className="field-label">{label}</div>
+				<div className="value" onClick={this.choose}>
+					{multiSelections ? 'Files' : 'File'}:&nbsp;
+					{
+						value
+							? (
+								multiSelections || value.length > 1
+									? <ul>{value.map((v, i) => <li key={i}>{v}</li>)}</ul>
+									: value[0]
+							)
+							: 'None'
+					}
+				</div>
+				<div className="bt" onClick={this.choose}>
+					{btLabel}
+				</div>
+				{help && <span className="help">{help}</span>}
+			</div>
+		);
 
-    get value() { //Always return an array
-        const { value } = this.props;
-        if (typeof(value) === "undefined")
-            return undefined;
-        return isArray(value) ? value : [value];
-    }
+	}
 
-    get label() {
-        return this.field.label;
-    }
+	get field() {
 
-    get type() {
-        return this.field.type;
-    }
+		return this.props.field;
 
-    get help() {
-        return this.field.help;
-    }
+	}
 
-    get filters() {
-        return this.field.filters || undefined;
-    }
+	get value() { // Always return an array
 
-    get multiSelections() {
-        return this.field.multiSelections || false;
-    }
+		const { value } = this.props;
+		if (typeof (value) === 'undefined') {
 
-    get showHiddenFiles() {
-        return this.field.showHiddenFiles || false;
-    }
+			return undefined;
 
-    get noResolveAliases() {
-        return this.field.noResolveAliases || false;
-    }
+		}
 
-    get treatPackageAsDirectory() {
-        return this.field.treatPackageAsDirectory || false;
-    }
+		return isArray(value) ? value : [ value ];
 
-    get dontAddToRecent() {
-        return this.field.dontAddToRecent || false;
-    }
+	}
 
-    get onChange() {
-        return this.props.onChange;
-    }
+	get label() {
 
-    choose() => {
-        const { multiSelections, showHiddenFiles, noResolveAliases, treatPackageAsDirectory, dontAddToRecent, filters } = this;
-        const properties = ['openFile'];
-        if (multiSelections)
-            properties.push("multiSelections");
-        if (showHiddenFiles)
-            properties.push("showHiddenFiles");
-        if (noResolveAliases)
-            properties.push("noResolveAliases");
-        if (treatPackageAsDirectory)
-            properties.push("treatPackageAsDirectory");
-        if (dontAddToRecent)
-            properties.push("dontAddToRecent");
+		return this.field.label;
 
-        const result = api.showOpenDialog({
-            properties: properties,
-            filters: filters
-        });
+	}
 
-        if (!result)
-            return;
+	get type() {
 
-        if (result.length) {
-            this.onChange(multiSelections ? result : result[0]);
-        }
-    }
+		return this.field.type;
+
+	}
+
+	get help() {
+
+		return this.field.help;
+
+	}
+
+	get filters() {
+
+		return this.field.filters || undefined;
+
+	}
+
+	get multiSelections() {
+
+		return this.field.multiSelections || false;
+
+	}
+
+	get showHiddenFiles() {
+
+		return this.field.showHiddenFiles || false;
+
+	}
+
+	get noResolveAliases() {
+
+		return this.field.noResolveAliases || false;
+
+	}
+
+	get treatPackageAsDirectory() {
+
+		return this.field.treatPackageAsDirectory || false;
+
+	}
+
+	get dontAddToRecent() {
+
+		return this.field.dontAddToRecent || false;
+
+	}
+
+	get onChange() {
+
+		return this.props.onChange;
+
+	}
+
+	choose() {
+
+		const { multiSelections, showHiddenFiles, noResolveAliases, treatPackageAsDirectory, dontAddToRecent, filters } = this;
+		const properties = [ 'openFile' ];
+		if (multiSelections) {
+
+			properties.push('multiSelections');
+
+		}
+
+		if (showHiddenFiles) {
+
+			properties.push('showHiddenFiles');
+
+		}
+
+		if (noResolveAliases) {
+
+			properties.push('noResolveAliases');
+
+		}
+
+		if (treatPackageAsDirectory) {
+
+			properties.push('treatPackageAsDirectory');
+
+		}
+
+		if (dontAddToRecent) {
+
+			properties.push('dontAddToRecent');
+
+		}
+
+		const result = api.showOpenDialog({
+			properties,
+			filters,
+		});
+
+		if (!result) {
+
+			return;
+
+		}
+
+		if (result.length) {
+
+			this.onChange(multiSelections ? result : result[0]);
+
+		}
+
+	}
 
 }
+
+FileField.propTypes = {
+	field: PropTypes.object,
+	value: PropTypes.string,
+	onChange: PropTypes.func,
+};
 
 export default FileField;
