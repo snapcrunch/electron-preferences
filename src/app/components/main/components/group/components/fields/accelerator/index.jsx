e3dbe2d14eef764ba8a6442db5371c562fd1c2ae
Lacy Morrow <me@lacymorrow.com>
2022-04-04 22:09:15 -0700

    fix #138: allow a single modifier as accelerator value

--- src/app/components/main/components/group/components/fields/accelerator/index.jsx
+++ src/app/components/main/components/group/components/fields/accelerator/index.jsx
@@ -23,19 +23,31 @@ const AcceleratorField = ({ field, value, onChange, key }) => {
 		like control, shift, etc. as well as a handful of oddball codes.
 		We only save if we receive one of these non-modifier keys.
 	*/
-	// const modifierKeyCodes = [16, 17, 18, 91, 92, 93]
+	const modifierKeyCodes = [16, 17, 18, 91, 92, 93]
 	const specialKeyCodes = new Set([ 0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 91, 92, 93, 94, 95 ]);
+
+
+		// Different OS' use different key names
+	let altKeyName = 'Alt'
+	let metaKeyName = 'Meta'
+	if (process.platform === 'darwin') {
+		altKeyName = 'Option'
+		metaKeyName = 'Command'
+	} else if (process.platform === 'win32') {
+		metaKeyName = 'Windows'
+	}
 
 	const handleKeyDown = event => {
 
 		event.preventDefault();
 
+		// Start key array with any modifiers
 		const keys = [
 			event.ctrlKey && 'Control',
 			event.metaKey && 'Command', // Probably should be called meta
 			event.altKey && 'Alt',
 			event.shiftKey && 'Shift',
-		].filter(Boolean);
+		].filter(Boolean); // remove false values
 
 		// I've not tested every combo to verify it will work in electron, all the documentation they provide:
 		// https://www.electronjs.org/docs/api/accelerator#available-key-codes
@@ -62,13 +74,17 @@ const AcceleratorField = ({ field, value, onChange, key }) => {
 			keys.push(keycodeToChar[event.which]);
 			onChange(keys.join('+'));
 
+		}  else if( field.allowOnlyModifier && modifierKeyCodes.has(event.which) && keys.length === 1) {
+			// `allowOnlyModifier`: we allow a single, modifier-only key to be set.
+			onChange(keys[0]);
+			return;
 		}
 
 		// Display current keys pressed
 		setPressing(true);
 		setAccelerator(keys.join('+'));
 
-	};
+	}
 
 	const handleKeyUp = _ => {
 
