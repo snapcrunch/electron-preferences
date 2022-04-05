18d20824394f316b0bdc68d406e4ca4aca5a89fd
Lacy Morrow <me@lacymorrow.com>
2022-04-05 10:30:16 -0700

    fix os check

--- src/app/components/main/components/group/components/fields/accelerator/index.jsx
+++ src/app/components/main/components/group/components/fields/accelerator/index.jsx
@@ -1,15 +1,16 @@
+/* global navigator */
 'use strict';
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import keycodeToChar from '../../../../../../../utils/keycodeToChar.js';
 
-const AcceleratorField = ({ field, value, onChange, key }) => {
+const AcceleratorField = ({ field, value, onChange }) => {
 
 	/*
 		AcceleratorField
 
-		props.field values: label, help, mofifierRequired
+		props.field values: label, help, modifierRequired
 
 		modifierRequired: By default any key can be used, if this is true, a modifier must be provided.
 	*/
@@ -23,18 +24,21 @@ const AcceleratorField = ({ field, value, onChange, key }) => {
 		like control, shift, etc. as well as a handful of oddball codes.
 		We only save if we receive one of these non-modifier keys.
 	*/
-	const modifierKeyCodes = [16, 17, 18, 91, 92, 93]
+	const modifierKeyCodes = [ 16, 17, 18, 91, 92, 93 ];
 	const specialKeyCodes = new Set([ 0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 91, 92, 93, 94, 95 ]);
 
+	// Assign modifier key names, different OS' use different key names
+	let altKeyName = 'Alt';
+	let metaKeyName = 'Meta';
+	if (navigator.userAgent.indexOf('Mac') !== -1) {
 
-		// Different OS' use different key names
-	let altKeyName = 'Alt'
-	let metaKeyName = 'Meta'
-	if (process.platform === 'darwin') {
-		altKeyName = 'Option'
-		metaKeyName = 'Command'
-	} else if (process.platform === 'win32') {
-		metaKeyName = 'Windows'
+		altKeyName = 'Option';
+		metaKeyName = 'Command';
+
+	} else if (navigator.userAgent.indexOf('Win') !== -1) {
+
+		metaKeyName = 'Windows';
+
 	}
 
 	const handleKeyDown = event => {
@@ -44,10 +48,10 @@ const AcceleratorField = ({ field, value, onChange, key }) => {
 		// Start key array with any modifiers
 		const keys = [
 			event.ctrlKey && 'Control',
-			event.metaKey && 'Command', // Probably should be called meta
-			event.altKey && 'Alt',
+			event.metaKey && metaKeyName, // Probably should be called meta
+			event.altKey && altKeyName,
 			event.shiftKey && 'Shift',
-		].filter(Boolean); // remove false values
+		].filter(Boolean); // Remove false values
 
 		// I've not tested every combo to verify it will work in electron, all the documentation they provide:
 		// https://www.electronjs.org/docs/api/accelerator#available-key-codes
@@ -74,17 +78,20 @@ const AcceleratorField = ({ field, value, onChange, key }) => {
 			keys.push(keycodeToChar[event.which]);
 			onChange(keys.join('+'));
 
-		}  else if( field.allowOnlyModifier && modifierKeyCodes.has(event.which) && keys.length === 1) {
+		} else if (field.allowOnlyModifier && modifierKeyCodes.has(event.which) && keys.length === 1) {
+
 			// `allowOnlyModifier`: we allow a single, modifier-only key to be set.
 			onChange(keys[0]);
+
 			return;
+
 		}
 
 		// Display current keys pressed
 		setPressing(true);
 		setAccelerator(keys.join('+'));
 
-	}
+	};
 
 	const handleKeyUp = _ => {
 
